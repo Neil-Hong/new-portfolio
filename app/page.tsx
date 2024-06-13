b2bd234dad6e0fab4b734813f948180c5370fd71
Neil-Hong <memoriaa94@gmail.com>
2024-06-14 01:52:33 +1000

    clinet moving card section complete

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
+import Clients from "@/components/ui/Clients";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
@@ -12,14 +14,11 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <h1>Neil's Portfolio</h1>
-        <FloatingNav className="" navItems={[{
-          name:"Home",
-          link:"/",
-          icon:<FaHome />
-        }]}/>
+        <FloatingNav className="" navItems={navItems}/>
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
