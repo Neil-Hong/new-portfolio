5fb487703a8feadb40c41b9a9a7819ae98128bac
Neil-Hong <memoriaa94@gmail.com>
2024-06-18 22:01:26 +1000

    change code based on vercel error message

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,7 +36,7 @@ const Footer = () => {
                     <div  className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
                       <img 
                           src={profile.img}
-                          alt={profile.id}
+                          alt={profile.img}
                           width={20}
                           height={20}
                       />
